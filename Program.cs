fcb911cec0e9f5ff7360b6d80c5ff168b4ffd5bc
اسماء انسان حامد زيد <Asmaa31221738@ics.tanta.edu.eg>
2025-12-27 02:53:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Principal;
+using Microsoft.VisualBasic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Transactions;
+using System.Xml.Linq;
+
+
+
+
+public class Account
+{
+    public string Name { get; set; }
+    public double Balance { get; set; }
+
+    public Account(string name = "Unnamed Account", double balance = 0.0)
+    {
+        this.Name = name;
+        this.Balance = balance;
+    }
+
+    public virtual bool Deposit(double amount)
+    {
+        if (amount < 0)
+            return false;
+        else
+        {
+            Balance += amount;
+            return true;
+        }
+    }
+
+    public virtual bool Withdraw(double amount)
+    {
+        if (Balance - amount >= 0)
+        {
+            Balance -= amount;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+public class SavingsAccount : Account
+{
+    public double InterestRate { get; set; }
+
+    public SavingsAccount(string name = "Unnamed Account", double balance = 0.0, double interestRate=0.0) : base(name, balance)
+    {
+
+
+        InterestRate = interestRate;
+    }
+    public override bool Deposit(double amount)
+    {
+        amount += amount * (InterestRate / 100);
+        return base.Deposit(amount);
+    }
+}
+public class CheckingAccount : Account
+{
+      double Fee = 1.50;
+
+    public CheckingAccount(string name = "Unnamed Checking Account",
+                           double balance = 0.0)
+        : base(name, balance)
+    {
+    }
+
+    public override bool Withdraw(double amount)
+    {
+        return base.Withdraw(amount + Fee);
+    }
+}
+public class TrustAccount : SavingsAccount
+{
+    private int withdrawCount = 0;
+    private  int MaxWithdrawals = 3;
+    private  double MaxWithdrawPercent = 0.20;
+
+    public TrustAccount(string name = "Unnamed Trust Account",
+                        double balance = 0.0,
+                        double interestRate = 0.0)
+        : base(name, balance, interestRate)
+    {
+    }
+
+    public override bool Deposit(double amount)
+    {
+        if (amount >= 5000)
+            amount += 50;
+
+        return base.Deposit(amount);
+    }
+
+    public override bool Withdraw(double amount)
+    {
+        if (withdrawCount >= MaxWithdrawals)
+            return false;
+
+        if (amount > Balance * MaxWithdrawPercent)
+            return false;
+
+        withdrawCount++;
+        return base.Withdraw(amount);
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        // Accounts
+        var accounts = new List<Account>();
+        accounts.Add(new Account());
+        accounts.Add(new Account("Larry"));
+        accounts.Add(new Account("Moe", 2000));
+        accounts.Add(new Account("Curly", 5000));
+
+        AccountUtil.Deposit(new List<Account>(accounts), 1000);
+        AccountUtil.Withdraw(new List<Account>(accounts), 2000);
+
+
+        var savAccounts = new List<SavingsAccount>();
+        savAccounts.Add(new SavingsAccount());
+        savAccounts.Add(new SavingsAccount("Superman"));
+        savAccounts.Add(new SavingsAccount("Batman", 2000));
+        savAccounts.Add(new SavingsAccount("Wonderwoman", 5000, 5.0));
+
+
+        AccountUtil.Deposit(new List<Account>(savAccounts), 1000);
+        AccountUtil.Withdraw(new List<Account>(savAccounts), 2000);
+
+
+        // Checking
+        var checAccounts = new List<CheckingAccount>();
+        checAccounts.Add(new CheckingAccount());
+        checAccounts.Add(new CheckingAccount("Larry2"));
+        checAccounts.Add(new CheckingAccount("Moe2", 2000));
+        checAccounts.Add(new CheckingAccount("Curly2", 5000));
+
+        AccountUtil.Deposit(new List<Account>(checAccounts), 1000);
+        AccountUtil.Withdraw(new List<Account>(checAccounts), 2000);
+
+        AccountUtil.Withdraw(new List<Account>(checAccounts), 2000);
+
+
+        // Trust
+        var trustAccounts = new List<TrustAccount>();
+        trustAccounts.Add(new TrustAccount());
+        trustAccounts.Add(new TrustAccount("Superman2"));
+        trustAccounts.Add(new TrustAccount("Batman2", 2000));
+        trustAccounts.Add(new TrustAccount("Wonderwoman2", 5000, 5.0));
+
+        AccountUtil.Deposit(new List<Account>(trustAccounts), 1000);
+        AccountUtil.Deposit(new List<Account>(trustAccounts), 6000);
+        AccountUtil.Withdraw(new List<Account>(trustAccounts), 2000);
+        AccountUtil.Withdraw(new List<Account>(trustAccounts), 3000);
+        AccountUtil.Withdraw(new List<Account>(trustAccounts), 500);
+    }
+
+        public static class AccountUtil
+    {
+
+        public static void Deposit(List<Account> accounts, double amount)
+        {
+            Console.WriteLine("\n=== Depositing =================================");
+            foreach (var acc in accounts)
+            {
+                if (acc.Deposit(amount))
+                    Console.WriteLine($"Deposited {amount} to {acc.Name}, Balance = {acc.Balance}");
+                else
+                    Console.WriteLine($"Failed Deposit of {amount} to {acc.Name}");
+            }
+        }
+
+        public static void Withdraw(List<Account> accounts, double amount)
+        {
+            Console.WriteLine("\n=== Withdrawing ================================");
+            foreach (var acc in accounts)
+            {
+                if (acc.Withdraw(amount))
+                    Console.WriteLine($"Withdrew {amount} from {acc.Name}, Balance = {acc.Balance}");
+                else
+                    Console.WriteLine($"Failed Withdrawal of {amount} from {acc.Name}");
+            }
+        }
+
+    }
+
+   }
+
+
+
+
+
+    
+
+
+    
+
+    
+    
+    
+    
+
+
+
+
